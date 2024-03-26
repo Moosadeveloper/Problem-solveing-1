5771be1f5a2437dd3902ab3801ab1a7fa374337a
Moosadeveloper <janmoosa77@gmail.com>
2024-03-26 20:15:29 +0500

    first commit

---
+++ chap1.js
@@ -0,0 +1,14 @@
+a="First name"
+alert(a);
+a="Last name"
+alert(a);
+a="Email"
+alert(a);
+a="Phone number"
+alert(a);
+a="password"
+alert(a);
+
+alert("You're learning JavaScript!");
+
+alert("Ramadan karim");
